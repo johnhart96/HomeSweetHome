b077f44ed445bf951ca84d69d5b6db46f1ad8018
John Hart <john@johnhart96.co.uk>
2020-06-17 11:50:16 +0100

    Making the install safer

--- install.sh
+++ install.sh
@@ -1,21 +1,24 @@
+cd ~
+
 apt-get update
 apt-get install git wakeonlan apache2 php7.3 libapache2-mod-php php7.3-sqlite -y
-
-echo 'www-data    ALL=(ALL:ALL) ALL' | tee -a /etc/sudoers
+path="`grep -i 'DocumentRoot' /etc/apache2/sites-available/000-default.conf`"
+www="${path/DocumentRoot /}"
 sed -i '11 b; s/AllowOverride None\b/AllowOverride All/' /etc/apache2/apache2.conf
 systemctl restart apache2
-chmod 777 -R /var/www/html
 rm -rf HomeSweetHome
-git clone https://github.com/johnhart96/HomeSweetHome/
-mv HomeSweetHome/* /var/www/html/
-rm -rf HomeSweetHome
-rm /var/www/html/index.html
-echo 'AuthUserFile /var/www/html/.htpasswd' >> /var/www/html/.htaccess
-echo 'AuthType Basic' >> /var/www/html/.htaccess
-echo 'AuthName "Please login"' >> /var/www/html/.htaccess
-echo 'Require valid-user' >> /var/www/html/.htaccess
+git clone https://github.com/johnhart96/HomeSweetHome/ $www
+mv $www/HomeSweetHome/* $www
+rm -rf $www/HomeSweetHome
+rm $www/index.html
+echo 'AuthUserFile /var/www/html/.htpasswd' >> $www/.htaccess
+echo 'AuthType Basic' >> $www/.htaccess
+echo 'AuthName "Please login"' >> $www/.htaccess
+echo 'Require valid-user' >> $www/.htaccess
 
-htpasswd -mc /var/www/html/.htpasswd admin
+htpasswd -mc $www/.htpasswd admin
+
+
 
 
 
